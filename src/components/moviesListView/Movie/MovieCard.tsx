6a3d68c7cdc3b16e985abe13d434ac69546a1ccd
serhiiCheremisin <sebanho@gmail.com>
2025-02-04 14:47:51 +0100

    feat: popup movie display: nev buttons have been added

--- src/components/moviesListView/Movie/MovieCard.tsx
+++ src/components/moviesListView/Movie/MovieCard.tsx
@@ -14,18 +14,17 @@ export default function MovieCard( { movie }: MovieCardProps ):JSX.Element {
      if (isPopupShowed) {
        return <MoviePopup movie={movie} closePopup={setIsPopupShowed} />
      }
-
-           return(
-           <button key={movie.id} className='movie-list--card' onClick={() => setIsPopupShowed(true)} tabIndex={0}>
-               <div className='movie-list--card-background' style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w300/${movie.poster_path})`,
-               backgroundPosition: 'center',
-               backgroundSize: 'cover',
-               backgroundRepeat: 'no-repeat'}}></div>
-               <div className='movie-list--card-info'>
-               <span><h3>{ movie.title }</h3></span>  
-               <span>{ movie.release_date }</span>  
-               <div className='movie-list--card-rating'> { parseFloat((Math.round(movie.vote_average * 100) / 100).toFixed(1)) } </div>
-               </div>
-           </button>
-     )
+      return(
+      <button key={movie.id} className='movie-list--card' onClick={() => setIsPopupShowed(true)} tabIndex={0}>
+          <div className='movie-list--card-background' style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w300/${movie.poster_path})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat'}}></div>
+          <div className='movie-list--card-info'>
+          <span><h3>{ movie.title }</h3></span>  
+          <span>{ movie.release_date }</span>  
+          <div className='movie-list--card-rating'> { parseFloat((Math.round(movie.vote_average * 100) / 100).toFixed(1)) } </div>
+          </div>
+      </button>
+)
 }
--- src/components/moviesListView/Movie/MoviePopup.tsx
+++ src/components/moviesListView/Movie/MoviePopup.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../../styles/movieView.css';
 import { IMovieListItem } from '../../../types/propsTypes';
+import useMoviesList from '../../../services/hooks/useMoviesList';
 
 interface MoviePopupProps {
     closePopup :  React.Dispatch<React.SetStateAction<boolean>>;
@@ -8,21 +9,40 @@ interface MoviePopupProps {
 }
 
 export default function MoviePopup( { closePopup, movie } : MoviePopupProps ):JSX.Element {
+
+ const [currentId, setCurrentId] = useState<number>(0);
+ const [currentMovie,setCurrentMovie] = useState<IMovieListItem>();
+
+ const movieList = useMoviesList();
+
+ useEffect(() => {
+  const index = movieList.results.findIndex(element => element.id === movie.id);
+  setCurrentId(index);
+ },[]);
+
+ useEffect(() => {
+  setCurrentMovie(movieList.results[currentId])
+ }, [currentId])
+
   return (
     <div className='movie-information-popup'>
-      <div className="movie-information-popup--big-poster" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w300/${movie.poster_path})`,
+      <div className="movie-information-popup--big-poster" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w300/${currentMovie?.poster_path})`,
                backgroundPosition: 'center',
                backgroundSize: 'contain',
                backgroundRepeat: 'no-repeat'}}></div>
       <div className="movie-information-popup--full-information">
         <button onClick={() => closePopup(false)} className='movie-information-popup--close-button'>X</button>
         <ul className='movie-information-popup--full-information__list'>
-          <li> <span>Title</span>: {movie.original_title}</li>
-          <li><span>Description</span>: {movie.overview}</li>
-          <li><span>Original language</span>: {movie.original_language}</li>
-          <li><span>Release date</span>: {movie.release_date}</li>
-          <li><span>Average rating</span>: {movie.vote_average}. <span>Based on</span>: {movie.vote_count} reviews</li>
+          <li> <span>Title</span>: {currentMovie?.original_title}</li>
+          <li><span>Description</span>: {currentMovie?.overview}</li>
+          <li><span>Original language</span>: {currentMovie?.original_language}</li>
+          <li><span>Release date</span>: {currentMovie?.release_date}</li>
+          <li><span>Average rating</span>: {currentMovie?.vote_average} <span>Based on</span>: {currentMovie?.vote_count} reviews</li>
         </ul>
+        <div className='movie-information-popup--navigation'>
+          <button disabled={currentId <= 0 ? true : false} onClick={() => setCurrentId(prev => prev-1)}>Previous</button>
+          <button disabled={currentId > movieList.results.length-2 ? true : false} onClick={() => setCurrentId(prev => prev+1)}>Next</button>
+        </div>
       </div>
     </div>
   )
